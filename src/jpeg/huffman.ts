91b0c709cc1890e508c896e1d0a626381b0f2064
lmriccardo <absintio098@gmail.com>
2025-03-23 18:30:31 +0100

    feat: :construction: Adds some new features (to be tested) for Huffman and utils
    Added HuffmanTable generation, BitStream class and some utilities functions

---
+++ src/jpeg/huffman.ts
@@ -0,0 +1,161 @@
+/**
+ * Represents a single node in the Huffman Tree. Each node will have two children.
+ * The left child is always reachable using a 0 input, while 1 for the rigth one.
+ * Moreover, a node is a leaf in the corresponding tree if it has a symbol not null
+ */
+export class HuffmanNode {
+  private left   : HuffmanNode | null = null; // The left child
+  private rigth  : HuffmanNode | null = null; // The rigth child
+  private symbol : number | null      = null; // The optional symbol 
+
+  constructor(symbol?: number) {
+    this.symbol = symbol ?? null;
+  }
+
+  private validateInput(bit: number) {
+    if (bit < 0 || bit > 1) throw new Error(
+      '[HuffmanNode:setNextNode:InvalidInput] Possible inputs are 0 or 1 ' +
+      `given input is ${bit}`);
+  }
+
+  /**
+   * Depending on the input bit (0/1) it will creates the
+   * corresponding child for the current node.
+   * 
+   * @param bit The input bit value
+   * @throws Error if the input value is different from 0 or 1
+   */
+  setNextNode(bit: number) {
+    this.validateInput(bit);
+    if (bit === 0 && !this.left) { this.left = new HuffmanNode() } 
+    else if (bit === 1 && !this.rigth) { this.rigth = new HuffmanNode() }
+  }
+
+  /**
+   * Returns the corresponding node given the input value
+   * @param bit The input bit either 0 or 1
+   * @returns The corresponding Huffman Node if exists, Null otherwise
+   */
+  getNextNode(bit: number) : HuffmanNode | null {
+    this.validateInput(bit);
+
+    switch (bit) {
+      case 0: return this.left;
+      case 1: return this.rigth;
+      default: return null;
+    }
+  }
+
+  /**
+   * Sets the corresponding symbol to the node
+   * @param symbol The input value for the symbol
+   */
+  setSymbol(symbol: number) {
+    // First we need to check that the symbol has not already
+    // been set before. Otherwise, raise an error
+    if (this.symbol) throw new Error('[HuffmanNode:setSymbol:AlreadyExists] ' +
+      `Impossible to set a new symbol, since already exists: ${this.symbol}`);
+
+    this.symbol = symbol;
+  }
+
+  /**
+   * Returns the symbol associated with the Node.
+   * @returns A value if the symbol exists, Null otherwise
+   */
+  getSymbol() : number | null { return this.symbol; }
+
+  /**
+   * Check if the current node is a leaf node or not.
+   * @returns True or False
+   */
+  isLeaf() : boolean { return this.symbol != null; }
+}
+
+/**
+ * The Huffman Tree used for decoding/encoding. Notes that this tree works
+ * for JPEG images, since it is created starting from the HuffmanTable
+ * specification. 
+ */
+export class JPEGHuffmanTree {
+  private bits    : number[] = []; // Each element is the number of symbols for that not bits
+  private symbols : number[] = []; // Symbols associated to each code
+  private codes   : number[] = []; // All possible codes for each bit length
+  
+  private root: HuffmanNode = new HuffmanNode(); // The root node of the tree
+
+  private generateCodes(bits: number[]) {
+    let curr_code = 0; // Initialize the current code to 0  
+    
+    for (let bit_idx = 0; bit_idx < bits.length; bit_idx++) {
+      const bit_count = bits[bit_idx];
+
+      // If there are codes for that number of bits
+      if (bit_count > 0) {
+        for (let idx = 0; idx < bit_count; idx++) {
+          this.codes.push(curr_code);
+          curr_code++;
+        }
+      } else {
+        this.codes.push(-1);
+      }
+
+      curr_code <<= 1;
+    }
+  }
+
+  private buildHuffmanTree() {
+    let [symbols_index, codes_index] = [0, 0];
+    let curr_node = this.root;
+
+    for (let bits_idx = 0; bits_idx < this.bits.length; bits_idx++) {
+      const bit_len = bits_idx + 1;
+
+      // If there are no codes for the current number of bits
+      if (this.bits[bits_idx] === 0) {
+        codes_index++;
+        continue;
+      }
+
+      // Otherwise, there are codes that needs to be added to the tree
+      for (let counter = 0; counter < this.bits[bits_idx]; counter++) {
+        const curr_code = this.codes[codes_index + counter];
+        const curr_symbol = this.symbols[codes_index + counter];
+
+        // Traverse the tree and add new nodes to it
+        for (let i = bit_len - 1; i > -1; i--) {
+          const bit = (curr_code >> i) & 1;
+          curr_node.setNextNode(bit);
+          curr_node = curr_node.getNextNode(bit)!;
+        }
+
+        curr_node.setSymbol(curr_symbol);
+        curr_node = this.root;
+      }
+
+      codes_index += this.bits[bits_idx];
+      symbols_index += this.bits[bits_idx];
+    }
+  }
+
+  /**
+   * Generate the Huffman Tree given the input vectors BITS and symbols.
+   * In the context of JPEG images, the huffman tree is generated starting
+   * from the definition of the Huffman Table which gives the BITS vector
+   * and the HUFFVAL (symbols) vector. 
+   * 
+   * @param bits    A vector of length 16, each element is the number of symbols for that size
+   * @param symbols The symbols associated with each length
+   */
+  buildTree(bits: number[], symbols: number[]) {
+    this.bits = bits;
+    this.symbols = symbols;
+    
+    // Generate the codes for the input bits and symbols
+    // and then build the HuffmanTree.
+    this.generateCodes(bits);
+    this.buildHuffmanTree();
+  }
+
+  
+}
---
+++ src/utils/math.ts
@@ -0,0 +1,80 @@
+/**
+ * Transform the input vector into a matrix of dimension (R,C), where
+ * R is the number of rows and C is the number of columns in a zig-zag manner.
+ * 
+ * EXAMPLE: V = [1,2,...,12] and (R=3, C=4). The resulting matrix will be
+ * 
+ *     [
+ *       [1, 2,  6,  7]
+ *       [3, 5,  8, 11],
+ *       [4, 9, 10, 12]
+ *     ]
+ * 
+ * @param values The vector with all values to be set in the resulting matrix
+ * @param nr     The total number of rows
+ * @param nc     The total number of columns
+ * 
+ * @returns The zigzag matrix
+ */
+export const ToZigZag = (values: number[], nr: number, nc: number) 
+: number[][] => 
+{
+  // Check if the total number of values can fit the matrix
+  if (values.length !== nr * nc) {
+    throw new Error('[ToZigZag:InvalidDimension] Input number of values ' +
+      `${values.length} do not fit matrix dimension ${nr * nc}.`);
+  }
+
+  const zigzag : number[][] = Array.from({ length: nr }, () => Array(nc).fill(0));
+  const min_dimension = Math.min(nr - 1, nc - 1); // Take the min dimension
+
+  let direction = -1; // The update position direction
+  let element_idx = 0; // Element index inside the input vector
+
+  // At some point we need to switch the way in which we compute the new
+  // positions. This is enabled using a drop down flag and the counter
+  let drop_down_cnt = (min_dimension % 2 === 0) ? 1 : 2;
+  let drop_down_flag = 0;
+
+  // Initial position will always be (0, 0)
+  let curr_position = [0, 0];
+  let last_curr_pos = curr_position;
+
+  zigzag[0][0] = values[0]; // Initialize the fist position with the first element
+
+  // This loop cycle for all diagonals. If we look at the indexes of the cells
+  // belonging to the same diagonal, we can see that their sum is always the same.
+  for (let diag_sum = 0; diag_sum < ((nr - 1) + (nc - 1)) + 1; diag_sum++) {
+    // If the sum of the indexes of the diagonal is grater then the minimum
+    // dimension, then we need to switch otherwise we would go out of bound.
+    if (diag_sum > min_dimension && !drop_down_flag) {
+      drop_down_flag = 1;
+    }
+
+    // Update the current position based on the direction and the drop down flag
+    if (((direction > 0) as any) ^ (drop_down_flag)) {
+      curr_position = [curr_position[0], curr_position[1] + 1];
+    } else {
+      curr_position = [curr_position[0] + 1, curr_position[1]];
+    }
+
+    // Loop through the current diagonal and assign the values
+    for (let idx = 0; idx < diag_sum + 1 - drop_down_flag * drop_down_cnt; idx++) {
+      const [r, c] = curr_position;
+      zigzag[r][c] = values[element_idx];
+      element_idx++;
+      last_curr_pos = curr_position;
+      curr_position = [curr_position[0] - direction, curr_position[1] + direction];
+    }
+
+    // Update the direction based on the parity of the current diagonal sum.
+    direction = (-1)**(diag_sum + 1);
+
+    // Also updates the drop down counter if the sum is less than min dimension
+    if (diag_sum > min_dimension) drop_down_cnt += 2;
+
+    curr_position = last_curr_pos;
+  }
+
+  return zigzag;
+}
